33dc1351fd645ed5dc73672bd61fc709684301a9
IamVatsal <vatsalpatel0609@gmail.com>
2025-03-11 00:19:36 +0530

    create Word_Frequency example.txt and main.py

---
+++ Word_Frequency/main.py
@@ -0,0 +1,32 @@
+from collections import Counter
+import re
+
+def word_frequency(text: str) -> list[tuple[str, int]]:
+    lowered_text = text.lower()
+    words = re.findall(r'\b\w+\b', lowered_text)
+    word_counter = Counter(words)
+    word_frequency = word_counter.most_common(n=10)
+    return word_frequency
+
+
+def main():
+    print("Welcome to the Word Frequency Counter I am going to count the frequency of words in a text file")
+    print("Showing the top 10 most frequent words")
+    
+    while True:
+        file_location = input("Path to the file: ")
+        try:
+            with open(file_location, "r") as file:
+                content = file.read()
+            word_count = word_frequency(content)
+            for word, frequency in word_count:
+                print(f"{word}: {frequency}")
+            break
+        except FileNotFoundError:
+            print("File not found. Please enter a valid path to the file.")
+        except Exception as e:
+            print(f"An error occurred: {e}")
+
+
+if __name__ == "__main__":
+    main()
